be27c14022be3483c8ebd235d517df629899eb33
Peter Zijlstra <peterz@infradead.org>
2017-05-06 16:42:08 +0200

    [BACKPORT] sched/fair: Remove se->load.weight from se->avg.load_sum
    DennySPb: Removed RT utilization tracking. It will be added later
    
    Remove the load from the load_sum for sched_entities, basically
    turning load_sum into runnable_sum.  This prepares for better
    reweighting of group entities.
    
    Since we now have different rules for computing load_avg, split
    ___update_load_avg() into two parts, ___update_load_sum() and
    ___update_load_avg().
    
    So for se:
    
      ___update_load_sum(.weight = 1)
      ___upate_load_avg(.weight = se->load.weight)
    
    and for cfs_rq:
    
      ___update_load_sum(.weight = cfs_rq->load.weight)
      ___upate_load_avg(.weight = 1)
    
    Since the primary consumable is load_avg, most things will not be
    affected. Only those few sites that initialize/modify load_sum need
    attention.
    
    Change-Id: I252c5ca64915c051de6c7b2dfd9477d4deaa13fa
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -560,8 +560,6 @@ struct rt_rq {
 	int overloaded;
 	struct plist_head pushable_tasks;
 
-	struct sched_avg avg;
-
 #endif /* CONFIG_SMP */
 	int rt_queued;
 
@@ -1867,7 +1865,6 @@ static inline int hrtick_enabled(struct rq *rq)
 
 #ifdef CONFIG_SMP
 extern void sched_avg_update(struct rq *rq);
-extern unsigned long sched_get_rt_rq_util(int cpu);
 
 #ifndef arch_scale_freq_capacity
 static __always_inline
