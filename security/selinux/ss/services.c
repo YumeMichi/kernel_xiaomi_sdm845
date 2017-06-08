7b9f7bf1fd6ece45e81fdff66fc3871b0930c098
Junil Lee <junil0814.lee@lge.com>
2017-06-08 13:18:09 +0900

    selinux: Use kmem_cache for ebitmap
    The allocated size for each ebitmap_node is 192byte by kzalloc().
    Then, ebitmap_node size is fixed, so it's possible to use only 144byte
    for each object by kmem_cache_zalloc().
    It can reduce some dynamic allocation size.
    
    Change-Id: Ibf082307c4645c91fff36e52bed51d907d1f88f7
    Signed-off-by: Junil Lee <junil0814.lee@lge.com>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -2068,9 +2068,11 @@ int security_load_policy(void *data, size_t len)
 
 	if (!ss_initialized) {
 		avtab_cache_init();
+		ebitmap_cache_init();
 		rc = policydb_read(&policydb, fp);
 		if (rc) {
 			avtab_cache_destroy();
+			ebitmap_cache_destroy();
 			goto out;
 		}
 
@@ -2081,6 +2083,7 @@ int security_load_policy(void *data, size_t len)
 		if (rc) {
 			policydb_destroy(&policydb);
 			avtab_cache_destroy();
+			ebitmap_cache_destroy();
 			goto out;
 		}
 
@@ -2088,6 +2091,7 @@ int security_load_policy(void *data, size_t len)
 		if (rc) {
 			policydb_destroy(&policydb);
 			avtab_cache_destroy();
+			ebitmap_cache_destroy();
 			goto out;
 		}
 
