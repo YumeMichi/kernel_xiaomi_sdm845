0232fbade921fa1fd7dbf698a17bea46d5d76be7
Thomas Gleixner <tglx@linutronix.de>
2017-06-20 01:37:47 +0200

    genirq: Introduce IRQD_MANAGED_SHUTDOWN
    Affinity managed interrupts should keep their assigned affinity accross CPU
    hotplug. To avoid magic hackery in device drivers, the core code shall
    manage them transparently. This will set these interrupts into a managed
    shutdown state when the last CPU of the assigned affinity mask goes
    offline. The interrupt will be restarted when one of the CPUs in the
    assigned affinity mask comes back online.
    
    Introduce the necessary state flag and the accessor functions.
    
    Change-Id: Iba688be5c0edf0179a93b58dcc34aac4c814630c
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Marc Zyngier <marc.zyngier@arm.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Keith Busch <keith.busch@intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Christoph Hellwig <hch@lst.de>
    Link: http://lkml.kernel.org/r/20170619235446.954523476@linutronix.de

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -206,6 +206,8 @@ struct irq_data {
  * IRQD_FORWARDED_TO_VCPU	- The interrupt is forwarded to a VCPU
  * IRQD_AFFINITY_MANAGED	- Affinity is auto-managed by the kernel
  * IRQD_IRQ_STARTED		- Startup state of the interrupt
+ * IRQD_MANAGED_SHUTDOWN	- Interrupt was shutdown due to empty affinity
+ *				  mask. Applies only to affinity managed irqs.
  */
 enum {
 	IRQD_TRIGGER_MASK		= 0xf,
@@ -224,6 +226,7 @@ enum {
 	IRQD_FORWARDED_TO_VCPU		= (1 << 20),
 	IRQD_AFFINITY_MANAGED		= (1 << 21),
 	IRQD_IRQ_STARTED		= (1 << 22),
+	IRQD_MANAGED_SHUTDOWN		= (1 << 23),
 };
 
 #define __irqd_to_state(d) ACCESS_PRIVATE((d)->common, state_use_accessors)
@@ -340,6 +343,11 @@ static inline void irqd_clr_activated(struct irq_data *d)
 static inline bool irqd_is_started(struct irq_data *d)
 {
 	return __irqd_to_state(d) & IRQD_IRQ_STARTED;
+}
+
+static inline bool irqd_is_managed_shutdown(struct irq_data *d)
+{
+	return __irqd_to_state(d) & IRQD_MANAGED_SHUTDOWN;
 }
 
 #undef __irqd_to_state
