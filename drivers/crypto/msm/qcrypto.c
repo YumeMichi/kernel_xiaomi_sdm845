84d1c6db3d9b9d116e49b0e02a7ce1d66a552c60
Miguel de Dios <migueldedios@google.com>
2018-10-31 03:04:37 -0700

    drivers: crypto: msm: qcrypto: Specify sync probe for qcrypto driver
    Bug: 115776306
    Bug: 77146523
    Change-Id: Ia1b95f962b348d11bc62776133e1c008467d576a
    Signed-off-by: Miguel de Dios <migueldedios@google.com>

--- drivers/crypto/msm/qcrypto.c
+++ drivers/crypto/msm/qcrypto.c
@@ -5459,6 +5459,7 @@ static struct platform_driver __qcrypto = {
 		.owner  = THIS_MODULE,
 		.name   = "qcrypto",
 		.of_match_table = qcrypto_match,
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 	},
 };
 
