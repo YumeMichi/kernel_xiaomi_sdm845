9235be5ff24102f352b40510a0e54e8fed0327fe
Bruno Martins <bgcngm@gmail.com>
2019-01-09 23:15:41 +0000

    drivers: drm: Expose high brightness mode (HBM) status
     * Only to let userspace be aware whether HBM is on or off.
       The params will still be set using Xiaomi's own API.
    
    Change-Id: Ic480bc13aad5f4378a0b92a53f89a33e6c33ce90

--- drivers/gpu/drm/drm_sysfs.c
+++ drivers/gpu/drm/drm_sysfs.c
@@ -386,6 +386,16 @@ static ssize_t mipi_reg_store(struct device *device,
 	return rc;
 }
 
+static ssize_t hbm_status_show(struct device *device,
+			   struct device_attribute *attr,
+			   char *buf)
+{
+	struct drm_connector *connector = to_drm_connector(device);
+	struct drm_device *dev = connector->dev;
+
+	return snprintf(buf, PAGE_SIZE, "%d\n", dev->hbm_status);
+}
+
 static DEVICE_ATTR_RW(status);
 static DEVICE_ATTR_RO(enabled);
 static DEVICE_ATTR_RO(dpms);
@@ -395,6 +405,7 @@ static DEVICE_ATTR_RW(disp_param);
 static DEVICE_ATTR_RO(doze_brightness);
 static DEVICE_ATTR_RW(disp_count);
 static DEVICE_ATTR_RW(mipi_reg);
+static DEVICE_ATTR_RO(hbm_status);
 
 static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_status.attr,
@@ -406,6 +417,7 @@ static struct attribute *connector_dev_attrs[] = {
 	&dev_attr_doze_brightness.attr,
 	&dev_attr_disp_count.attr,
 	&dev_attr_mipi_reg.attr,
+	&dev_attr_hbm_status.attr,
 	NULL
 };
 
