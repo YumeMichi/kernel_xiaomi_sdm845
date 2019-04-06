7837458d4208c0186deac94e71e8d4ecf50c7f9f
Danny Lin <danny@kdrag0n.dev>
2019-04-06 15:53:35 -0700

    exec: Fix potential race condition in boost task checker
    A race condition where the task's comm is updated while it is being
    compared may occur because we check the comm directly. Fix this by using
    the get_task_comm helper which copies the comm for comparison.
    
    Additionally, move and rename the checker function to reflect the change
    in arguments.
    
    Change-Id: I092657e2f58a479ff9e3ad70e5369840f8239854
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -123,6 +123,14 @@ extern void install_exec_creds(struct linux_binprm *bprm);
 extern void set_binfmt(struct linux_binfmt *new);
 extern ssize_t read_code(struct file *, unsigned long, loff_t, size_t);
 extern bool task_is_zygote(struct task_struct *p);
-extern bool is_boost_comm(char *comm);
+
+static inline bool tsk_is_booster(struct task_struct *tsk)
+{
+	char comm[sizeof(tsk->comm)];
+
+	get_task_comm(comm, tsk);
+	return !strcmp(comm, "init") ||
+	       !strcmp(comm, "power@1.0-servi");
+}
 
 #endif /* _LINUX_BINFMTS_H */
