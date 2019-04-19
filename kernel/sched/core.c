bb81a924b630d28bbe3a9bc632ff330ed0fc40ed
Danny Lin <danny@kdrag0n.dev>
2019-04-18 21:24:06 -0700

    treewide: Update code from Sultan Alsawaf
    This brings all the code that we have from Sultan up to date as of the
    following commit: https://github.com/kerneltoast/android_kernel_google_wahoo/commit/45435ddeee1b31aaad29fa7fc819446ef6bb0044
    
    Appropriate adjustments for our tree have been applied, such as:
      - Revamping wake boost handling in devfreq_boost for the new
      serialized boost thread
      - Adapting fb_notifier callback edits for our msm_drm_notifier
      callbacks
      - Updating is_lmkd_pid -> task_is_lmkd in accordance with the
      is_zygote_pid -> task_is_zygote update
    
    Test: cpubw, cpufreq, and stune boosting work correctly as reported by
    sysfs
    Test: lmkd optimizations and zygote boosts trigger appropriately
    Test: WireGuard is re-downloaded after being deleted
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    
    Change-Id: Ie1a6818f1e7704aff23b76855076e6ab0546eb5f

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1119,8 +1119,8 @@ void set_cpus_allowed_common(struct task_struct *p, const struct cpumask *new_ma
 	p->nr_cpus_allowed = cpumask_weight(new_mask);
 }
 
-static const struct cpumask *
-get_adjusted_cpumask(struct task_struct *p, const struct cpumask *orig_mask)
+static const struct cpumask *adjust_cpumask(struct task_struct *p,
+					    const struct cpumask *req_mask)
 {
 	/* Force all performance-critical kthreads onto the big cluster */
 	if (p->flags & PF_PERF_CRITICAL)
@@ -1128,10 +1128,10 @@ get_adjusted_cpumask(struct task_struct *p, const struct cpumask *orig_mask)
 
 	/* Force all trivial, unbound kthreads onto the little cluster */
 	if (p->flags & PF_KTHREAD && !is_global_init(p) &&
-	    cpumask_equal(orig_mask, cpu_all_mask))
+	    cpumask_equal(req_mask, cpu_all_mask))
 		return cpu_lp_mask;
 
-	return orig_mask;
+	return req_mask;
 }
 
 void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
@@ -1139,7 +1139,7 @@ void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 	struct rq *rq = task_rq(p);
 	bool queued, running;
 
-	new_mask = get_adjusted_cpumask(p, new_mask);
+	new_mask = adjust_cpumask(p, new_mask);
 
 	lockdep_assert_held(&p->pi_lock);
 
@@ -1184,7 +1184,7 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	int ret = 0;
 	cpumask_t allowed_mask;
 
-	new_mask = get_adjusted_cpumask(p, new_mask);
+	new_mask = adjust_cpumask(p, new_mask);
 
 	rq = task_rq_lock(p, &rf);
 	update_rq_clock(rq);
