a4c33686504d2bf2ce20cbfb50246bd142d9926a
未霜 <admin@example.com>
2019-05-13 11:54:45 +0800

    msm: thermal: simple: Allow userplace to disable thermal driver
    Change-Id: I1d6f25dc9dabc74bdfedcdc07547b60fe3d1c2df

--- drivers/thermal/qcom/msm_thermal_simple.c
+++ drivers/thermal/qcom/msm_thermal_simple.c
@@ -8,6 +8,7 @@
 #include <linux/cpu.h>
 #include <linux/cpufreq.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/slab.h>
@@ -20,6 +21,9 @@
 		pr_err("%s: " prop " property missing\n", (node)->name);	\
 	ret;									\
 })
+
+bool enable_thermal = true;
+module_param(enable_thermal, bool, 0644);
 
 struct thermal_zone {
 	u32 gold_khz;
@@ -72,12 +76,16 @@ static void thermal_throttle_worker(struct work_struct *work)
 	old_zone = t->curr_zone;
 	new_zone = NULL;
 
-	for (i = t->nr_zones - 1; i >= 0; i--) {
-		if (temp_deg >= t->zones[i].trip_deg) {
-			new_zone = t->zones + i;
-			break;
+	if(enable_thermal) {
+		for (i = t->nr_zones - 1; i >= 0; i--) {
+			if (temp_deg >= t->zones[i].trip_deg) {
+				new_zone = t->zones + i;
+				break;
+			}
 		}
 	}
+	else
+		new_zone = NULL;
 
 	/* Update thermal zone if it changed */
 	if (new_zone != old_zone) {
