LineageOS/android_kernel_xiaomi_sdm845

drivers: usb: Import Xiaomi changes for USB audio
Change-Id: I4dbf44995e2bc05dd632fc33f543f56415f9b33f

drivers/usb/core/hub.c
@@ -108,6 +108,10 @@ static void hub_release(struct kref *kref);
 static int usb_reset_and_verify_device(struct usb_device *udev);
 static int hub_port_disable(struct usb_hub *hub, int port1, int set_state);
 
+unsigned int connected_usb_idVendor = 0;
+unsigned int connected_usb_idProduct = 0;
+unsigned int connected_usb_devnum = 0xff;
+
 static inline char *portspeed(struct usb_hub *hub, int portstatus)
 {
 	if (hub_is_superspeedplus(hub->hdev))
@@ -2111,6 +2115,13 @@ void usb_disconnect(struct usb_device **pdev)
 	dev_info(&udev->dev, "USB disconnect, device number %d\n",
 			udev->devnum);
 
+	if (connected_usb_devnum == udev->devnum) {
+		dev_info(&udev->dev, "xiaomi headset removed, devnum %d\n", udev->devnum);
+		connected_usb_idVendor = 0;
+		connected_usb_idProduct = 0;
+		connected_usb_devnum = 0xff;
+	}
+
 	/*
 	 * Ensure that the pm runtime code knows that the USB device
 	 * is in the process of being disconnected.
@@ -2437,6 +2448,14 @@ int usb_new_device(struct usb_device *udev)
 	/* export the usbdev device-node for libusb */
 	udev->dev.devt = MKDEV(USB_DEVICE_MAJOR,
 			(((udev->bus->busnum-1) * 128) + (udev->devnum-1)));
+
+	if ((0x2717 == le16_to_cpu(udev->descriptor.idVendor))
+			 && (0x3801 == le16_to_cpu(udev->descriptor.idProduct))) {
+		connected_usb_idVendor = le16_to_cpu(udev->descriptor.idVendor);
+		connected_usb_idProduct = le16_to_cpu(udev->descriptor.idProduct);
+		connected_usb_devnum = udev->devnum;
+		dev_info(&udev->dev, "xiaomi headset identified, devnum %d\n", udev->devnum);
+	}
 
 	/* Tell the world! */
 	announce_device(udev);

YumeMichi/kernel_xiaomi_sdm845

drivers/usb/core/hub.c
@@ -5,6 +5,7 @@
  * (C) Copyright 1999 Johannes Erdfelt
  * (C) Copyright 1999 Gregory P. Smith
  * (C) Copyright 2001 Brad Hards (bhards@bigpond.net.au)
+ * Copyright (C) 2018 XiaoMi, Inc.
  *
  */
 
@@ -107,6 +108,10 @@ EXPORT_SYMBOL_GPL(ehci_cf_port_reset_rwsem);
 static void hub_release(struct kref *kref);
 static int usb_reset_and_verify_device(struct usb_device *udev);
 static int hub_port_disable(struct usb_hub *hub, int port1, int set_state);
+
+unsigned int connected_usb_idVendor = 0;
+unsigned int connected_usb_idProduct = 0;
+unsigned int connected_usb_devnum = 0xff;
 
 static inline char *portspeed(struct usb_hub *hub, int portstatus)
 {
@@ -2130,6 +2135,13 @@ void usb_disconnect(struct usb_device **pdev)
 	dev_info(&udev->dev, "USB disconnect, device number %d\n",
 			udev->devnum);
 
+	if (connected_usb_devnum == udev->devnum) {
+		dev_info(&udev->dev, "Xiaomi headset removed, devnum %d\n", udev->devnum);
+		connected_usb_idVendor = 0;
+		connected_usb_idProduct = 0;
+		connected_usb_devnum = 0xff;
+	}
+
 	/*
 	 * Ensure that the pm runtime code knows that the USB device
 	 * is in the process of being disconnected.
@@ -2456,6 +2468,14 @@ int usb_new_device(struct usb_device *udev)
 	/* export the usbdev device-node for libusb */
 	udev->dev.devt = MKDEV(USB_DEVICE_MAJOR,
 			(((udev->bus->busnum-1) * 128) + (udev->devnum-1)));
+
+	if ((0x2717 == le16_to_cpu(udev->descriptor.idVendor))
+			 && (0x3801 == le16_to_cpu(udev->descriptor.idProduct))) {
+		connected_usb_idVendor = le16_to_cpu(udev->descriptor.idVendor);
+		connected_usb_idProduct = le16_to_cpu(udev->descriptor.idProduct);
+		connected_usb_devnum = udev->devnum;
+		dev_info(&udev->dev, "Xiaomi headset identified, devnum %d\n", udev->devnum);
+	}
 
 	/* Tell the world! */
 	announce_device(udev);