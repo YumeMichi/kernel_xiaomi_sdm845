4820b6f620008ad336447d45731f8d8dc62631e2
tanish2k09 <tanish2k09.dev@gmail.com>
2019-05-30 18:22:06 -0700

    drm/msm: klapse: v5.0
    - Introducing k-lapse "flow", a brightness-based scaling mode QOL enhancement that scales RGB points by 1 smoothly instant of instantly taking effect
    - Major code refactor to better match the Linux Kernel coding style (huge thanks to kdrag0n for initial overall patch and general guidance)
    - Switched from SYS ATTR to module_param_cb and kernel param structs
    - Shortened the code from 1k lines to 0.6k without compromising any logic or feature
    - Changed tunable naming for sysfs to convey the idea of the function in a better way
    - Added minute-level support for automation. Yes, you can now start or stop at, say 16:49
    - Dimmer factor (previously known as brightness_factor) is now allowed to be set as a percentage value, ranging from 10 to 100
    - Updated README documentation to match v5.0 release
    - Integrated MDSS code with a switch in klapse.h (see kcal-mdss-demo branch for full implementation in older version)
    - Changed some default config values
    - Revised dimmer logic to fix "dimmer value multiplicative effect" for some users. The issue was changing brightness_factor values multiple times stacked them on top of each other, eventually causing the RGB to hit their minimum limit (usually 20 in KCAL), and not being able to restore full RGB without a pulse
    - (self tag for notification?) tanish2k09
    
    Change-Id: Ie6e85521f050ff4038325dd2dca8b38b0176af2d

--- drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
+++ drivers/gpu/drm/msm/sde/sde_hw_color_proc_v4.c
@@ -15,9 +15,17 @@
 #include "sde_hw_color_proc_common_v4.h"
 #include "sde_hw_color_proc_v4.h"
 
+#ifdef CONFIG_KLAPSE
+#include <linux/klapse.h>
+
 unsigned short kcal_red = 256;
 unsigned short kcal_green = 256;
 unsigned short kcal_blue = 256;
+#else
+static unsigned short kcal_red = 256;
+static unsigned short kcal_green = 256;
+static unsigned short kcal_blue = 256;
+#endif
 static unsigned short kcal_hue = 0;
 static unsigned short kcal_sat = 255;
 static unsigned short kcal_val = 255;
@@ -30,10 +38,6 @@ module_param(kcal_hue, short, 0644);
 module_param(kcal_sat, short, 0644);
 module_param(kcal_val, short, 0644);
 module_param(kcal_cont, short, 0644);
-
-#ifdef CONFIG_KLAPSE
-#include "klapse.h"
-#endif
 
 static int sde_write_3d_gamut(struct sde_hw_blk_reg_map *hw,
 		struct drm_msm_3d_gamut *payload, u32 base,
