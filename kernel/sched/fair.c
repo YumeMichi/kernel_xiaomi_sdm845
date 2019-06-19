680a3930c15cf3ab2b283530aebb5015a9bd3177
Zachariah Kennedy <zkennedy87@gmail.com>
2019-06-19 04:43:40 +0000

    sched/fair: Add tracing to track stune boosted task placement
    Change-Id: Ib38992cc32d1be15caff1bb5f8b2a1eec0f37865
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7127,7 +7127,7 @@ retry:
 							prefer_idle, min_util,
 							cpu, best_idle_cpu,
 							best_active_cpu,
-							i, -1);
+							i, -1, boosted);
 
 					return i;
 				}
@@ -7376,7 +7376,7 @@ retry:
 
 	trace_sched_find_best_target(p, prefer_idle, min_util, cpu,
 				     best_idle_cpu, best_active_cpu,
-				     target_cpu, *backup_cpu);
+				     target_cpu, *backup_cpu, boosted);
 
 	schedstat_inc(p->se.statistics.nr_wakeups_fbt_count);
 	schedstat_inc(this_rq()->eas_stats.fbt_count);
@@ -7653,7 +7653,8 @@ unlock:
 	trace_sched_task_util(p, next_cpu, backup_cpu, target_cpu,
 			      fbt_env.need_idle, fastpath,
 			      fbt_env.placement_boost, rtg_target ?
-			      cpumask_first(rtg_target) : -1, start_t);
+			      cpumask_first(rtg_target) : -1, start_t,
+			      boosted, sync_boost);
 	return target_cpu;
 }
 
