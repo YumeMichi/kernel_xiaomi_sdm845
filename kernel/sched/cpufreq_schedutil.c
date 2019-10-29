0339da6bb3d2ad8fdcf22db12c16b11f7789e6ca
YumeMichi <do4suki@gmail.com>
2019-10-30 02:15:05 +0800

    Revert "cpufreq: schedutil: Update warn_on with bug_on"
    This reverts commit 322ea72269323002b4e3b6e19cfada862267a689.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -328,7 +328,7 @@ static void sugov_calc_avg_cap(struct sugov_policy *sg_policy, u64 curr_ws,
 	if (unlikely(!sysctl_sched_use_walt_cpu_util))
 		return;
 
-	BUG_ON(curr_ws < last_ws);
+	WARN_ON(curr_ws < last_ws);
 	if (curr_ws <= last_ws)
 		return;
 
