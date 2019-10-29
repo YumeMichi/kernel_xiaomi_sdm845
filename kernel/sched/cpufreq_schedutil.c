631a24fc5067314841293aa5e539624d97f2d200
YumeMichi <do4suki@gmail.com>
2019-10-30 02:19:40 +0800

    Revert "cpufreq: schedutil: Hardcode iowait_boost_enable"
    This reverts commit 27ca0f4efd078e81c4d552e2a6ae36f08a067915.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -968,7 +968,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->down_rate_limit_us *= lat;
 	}
 
-	tunables->iowait_boost_enable = true;
+	tunables->iowait_boost_enable = false;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
