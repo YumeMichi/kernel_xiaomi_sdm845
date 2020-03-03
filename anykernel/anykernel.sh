1205f5d23c04f42fde20dc9918272eef0956a3cc
YumeMichi <do4suki@gmail.com>
2020-03-03 09:07:32 +0800

    AK3: Adapted to Polar Kernel
    Change-Id: I92d380ba95806d70bda9e3b307e7f01c684f5c9e
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- anykernel/anykernel.sh
+++ anykernel/anykernel.sh
@@ -2,63 +2,44 @@
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
-# begin properties
+# Begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Polar Kernel by YumeMichi @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
+device.name1=dipper
+supported.versions=10
 supported.patchlevels=
-'; } # end properties
+'; } # End properties
 
-# shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+# Shell variables
+block=boot;
+is_slot_device=auto;
 ramdisk_compression=auto;
 
-
 ## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
+# Import patching functions/variables - see for reference
 . tools/ak3-core.sh;
-
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-
 ## AnyKernel install
+ui_print " " "Decompressing boot image..."
 dump_boot;
 
-# begin ramdisk changes
+# Begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# End ramdisk changes
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
-# end ramdisk changes
-
+ui_print " " "Installing new boot image..."
 write_boot;
-## end install
 
+## End install
+ui_print " " "Done!"
+$bb umount /system
