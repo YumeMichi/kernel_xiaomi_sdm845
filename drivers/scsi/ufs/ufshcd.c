0d07d1a64530e6f9b058ea8c5b94fa5ff4268ef1
Jaegeuk Kim <jaegeuk@google.com>
2020-03-22 16:27:12 -0700

    scsi: ufs: set autohibern8 timer regardless of hibern8_on_idle
    This fixes disabling autohibern8 by the previous commit.
    
    Bug: 151321015
    Bug: 152116476
    Fixes: 5a57ae46dd6 ("scsi: ufs: disable hibern8_on_idle")
    Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>
    Change-Id: If71f3212081f62d9c92b8f611aadb4f547ced1e0
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -1983,6 +1983,9 @@ static void ufshcd_exit_clk_gating(struct ufs_hba *hba)
 
 static void ufshcd_set_auto_hibern8_timer(struct ufs_hba *hba, u32 delay)
 {
+	if (!ufshcd_is_auto_hibern8_supported(hba))
+		return;
+
 	ufshcd_rmwl(hba, AUTO_HIBERN8_TIMER_SCALE_MASK |
 			 AUTO_HIBERN8_IDLE_TIMER_MASK,
 			AUTO_HIBERN8_TIMER_SCALE_1_MS | delay,
@@ -2327,6 +2330,7 @@ static void ufshcd_init_hibern8_on_idle(struct ufs_hba *hba)
 		 * auto hibern8 is supported
 		 */
 		hba->caps &= ~UFSHCD_CAP_HIBERN8_ENTER_ON_IDLE;
+		return;
 	} else {
 		hba->hibern8_on_idle.delay_ms = 10;
 		INIT_DELAYED_WORK(&hba->hibern8_on_idle.enter_work,
@@ -8158,9 +8162,7 @@ static int ufshcd_probe_hba(struct ufs_hba *hba)
 	 * Enable auto hibern8 if supported, after full host and
 	 * device initialization.
 	 */
-	if (ufshcd_is_auto_hibern8_supported(hba))
-		ufshcd_set_auto_hibern8_timer(hba,
-				      hba->hibern8_on_idle.delay_ms);
+	ufshcd_set_auto_hibern8_timer(hba, hba->hibern8_on_idle.delay_ms);
 out:
 	if (ret) {
 		ufshcd_set_ufs_dev_poweroff(hba);
