6efbf40b613ca7541fad1a062c446bb13b74d8af
Taniya Das <tdas@codeaurora.org>
2020-05-29 10:46:37 +0530

    cpuidle: lpm-levels: Fix regulator and clock prints in the suspend path
    Move the clock/regulator dumps to LPM suspend entry to avoid the
    possibility of scheduling while in atomic context as a mutex needs to
    be acquired before dumping the enabled clocks list.
    
    Change-Id: Ic3d65d78a3896aa969c759fc45193f3a4e3e772c
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    [PA 4.9 port note: regulator print didn't exist before, so this only
     affects clock print]

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1042,15 +1042,6 @@ static int cluster_configure(struct lpm_cluster *cluster, int idx,
 	}
 
 	if (level->notify_rpm) {
-		/*
-		 * Print the clocks which are enabled during system suspend
-		 * This debug information is useful to know which are the
-		 * clocks that are enabled and preventing the system level
-		 * LPMs(XO and Vmin).
-		 */
-		if (!from_idle)
-			clock_debug_print_enabled(false);
-
 		cpu = get_next_online_cpu(from_idle);
 		cpumask_copy(&cpumask, cpumask_of(cpu));
 
@@ -1669,6 +1660,15 @@ static int lpm_suspend_enter(suspend_state_t state)
 		pr_err("Failed suspend\n");
 		return 0;
 	}
+
+	/*
+	 * Print the clocks which are enabled during system suspend
+	 * This debug information is useful to know which are the
+	 * clocks that are enabled and preventing the system level
+	 * LPMs(XO and Vmin).
+	 */
+	clock_debug_print_enabled(false);
+
 	cpu_prepare(lpm_cpu, idx, false);
 	cluster_prepare(cluster, cpumask, idx, false, 0);
 
